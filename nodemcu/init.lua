fddcd0462e9d58445b166e1fc207a4f4f842b03e
Michal Sedlak <michal6103@gmail.com>
2015-10-28 18:54:06 +0100

    better readibility

--- nodemcu/init.lua
+++ nodemcu/init.lua
@@ -7,8 +7,13 @@ TIMER_NETWORK = 0   -- Timer used for network event
 TIMER_COLOR = 1     -- Timer used for color change
 TIMER_TIME = 2      -- Timer used for gettin time regularely
 
-function parseTime(pl)    
-    return string.match(pl, "%c(%d+):")    
+MODE_MORNING = CONFIG.MODE_MORNING
+MODE_DAY = CONFIG.MODE_DAY
+MODE_EVENING = CONFIG.MODE_EVENING
+MODE_NIGHT = CONFIG.MODE_NIGHT
+
+function parseTime(pl)
+    return string.match(pl, "%c(%d+):")
 end
 
 function getTimeCallback()
@@ -64,20 +69,14 @@ function rotateColorsCallback()
 	elseif time >= MODE_DAY then
     	s = 127
 		v = 255
-	end
-
-
-	if time >= MODE_EVENING then
+	elseif time >= MODE_EVENING then
 		s = 255
 		v = 127
-	end
-
-	if time >= MODE_NIGHT then
+	elseif time >= MODE_NIGHT then
 		s = 255
 		v = 63
 	end
 
-    hue = hue + 1
 	-- in evening and night there is no  blue and minimal green
 	if time >= MODE_EVENING or time >= MODE_NIGHT then
 		if  hue > 21 then
@@ -93,6 +92,7 @@ function rotateColorsCallback()
     -- print("RGB",r,g,b)
     strip:setRGB(r, g, b)
 
+    hue = hue + 1
 end
 
 
